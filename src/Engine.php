da2e954bdbe143dd36a905cc55ce37d2733c8461
Alexander Tokarev <kosades.dev@gmail.com>
2021-02-10 23:39:24 +0600

    Rework main function

--- src/Engine.php
+++ src/Engine.php
@@ -4,15 +4,25 @@ namespace Gendiff;
 
 class Engine
 {
-    public static function compareJsonFiles($firstFile, $secondFile)
+    public static function diff($firstFile, $secondFile)
+    {
+        [$firstFileData, $secondFileData] = self::getDataFromFiles($firstFile, $secondFile);
+        $diff = self::calculateDiff($firstFileData, $secondFileData);
+        self::printResult($diff);
+    }
+
+    private static function getDataFromFiles($firstFile, $secondFile): array
     {
         $firstFileData = json_decode(file_get_contents($firstFile), true);
         $secondFileData = json_decode(file_get_contents($secondFile), true);
+        return [$firstFileData, $secondFileData];
+    }
 
+    private static function calculateDiff($firstFileData, $secondFileData): array
+    {
         $keys = array_merge(array_keys($firstFileData), array_keys($secondFileData));
         sort($keys);
-
-        $diff = array_reduce($keys, function ($carry, $key) use ($firstFileData, $secondFileData) {
+        return array_reduce($keys, function ($carry, $key) use ($firstFileData, $secondFileData) {
             if (!isset($firstFileData[$key])) {
                 $carry["+ $key"] = $secondFileData[$key];
             } elseif (!isset($secondFileData[$key])) {
@@ -25,7 +35,10 @@ class Engine
             }
             return $carry;
         }, []);
+    }
 
+    private static function printResult($diff)
+    {
         foreach ($diff as $key => $value) {
             if ($value === false) {
                 $str = 'false';
